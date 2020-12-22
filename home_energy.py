7aac04c57ac172392442e712f9842f3944dd6f17
Xiao Wang <xywang84@gmail.com>
2020-12-22 09:51:45 -0500

    home_energy updated

--- home_energy.py
+++ home_energy.py
@@ -36,6 +36,13 @@ def get_gsheet(sname):
     df["Date/Time"] = df["Date/Time"].transform(lambda x: datetime.datetime.fromtimestamp(x/1000))
     return df
 
+def process_inverter_log(fname):
+    log_base = pd.read_csv(fname)
+    new_log = pd.DataFrame({"Date/Time": log_base.iloc[:,0], "Event Value": log_base.iloc[:,1]})
+    new_log["Date/Time"] = new_log["Date/Time"].transform(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d %H:%M") )
+    return new_log
+    
+    
 def seperateClampVals(dataframe):
     c1vals = dataframe[dataframe["Device"].str.match("Aeon.*")]
     if(c1vals.empty):
@@ -95,13 +102,29 @@ def return_start_elms(sol_log):
     # filter empty results
     return [i for i in match if i]
 
-def daily_generation(sol_log):
+def return_end_elms(sol_log):
+    #returns the first entry of the day
+    start_date = datetime.datetime(sol_log[0][0].year, sol_log[0][0].month,sol_log[0][0].day)
+    end_date = datetime.datetime(sol_log[-1][0].year, sol_log[-1][0].month,sol_log[-1][0].day)
+    lst = []
+    while(start_date <= end_date):        
+        lst.append(return_log(sol_log,cday=start_date)[-1])
+        start_date+=datetime.timedelta(days=1)
+    # filter empty results
+    return lst
+
+def daily_generation(sol_log, defaultmode=True):
     start_log = return_start_elms(sol_log)
     daily_gen = []
     # daily solar generation will be returned as a date-range, whcih is simply merges the 
-    for i in range(1,len(start_log)):
-        daily_gen.append([[start_log[i-1][0], start_log[i][0]], start_log[i][1] - start_log[i-1][1]])
-    daily_gen.append([[start_log[-1][0], sol_log[-1][0]], sol_log[-1][1] - start_log[-1][1]])
+    if(defaultmode):
+        for i in range(1,len(start_log)):
+            daily_gen.append([[start_log[i-1][0], start_log[i][0]], start_log[i][1] - start_log[i-1][1]])    
+        daily_gen.append([[start_log[-1][0], sol_log[-1][0]], sol_log[-1][1] - start_log[-1][1]])
+    else:
+        for i in range(1,len(start_log)):
+            daily_gen.append([[start_log[i-1][0], start_log[i][0]], start_log[i][1]])    
+        daily_gen.append([[start_log[-1][0], sol_log[-1][0]], sol_log[-1][1]])
     return daily_gen
 
 def total_consumption(sol_log, fname=False):
@@ -137,9 +160,35 @@ def plot_consumption(sol_log):
     return fig
     # fig3.add_trace(go.Bar(x=[i[0][0] for i in weekly_g
 
-def return_log(sol_log, cday = datetime.datetime.today()):
-    # cday = datetime.date.today();
+def return_log(sol_log, cday = None):
+    if cday is None:
+        cday = datetime.datetime.today()
     if(isinstance(cday, list)):
         return [i for i in sol_log if i[0] >= cday[0] and i[0] <= cday[1] and i[-1] > 0 and i[-1] < 20000]
     else:
         return [i for i in sol_log if i[0].day == cday.day and i[0].month == cday.month and i[-1] > 0  and i[-1] < 20000]      
+
+def read_ecobee(ecobee_csv_file):
+    thermDataHeader = pd.read_csv(ecobee_csv_file, skiprows=4).keys()    
+    thermDataData = pd.read_csv(ecobee_csv_file, skiprows=6)
+    res = {kk:thermDataData.iloc[:,idx] for idx,kk in enumerate(thermDataHeader)}
+    res["Datetime"] = (res["Date"]+ " "+res["Time"]).transform(lambda x: datetime.datetime.strptime(x, "%Y-%m-%d %H:%M:%S") )
+    ecobeeData = pd.DataFrame(res)
+    return ecobeeData
+
+def return_ecobee_data(ecobeeData, cday=None):
+    if cday is None:
+        cday = datetime.datetime.today()
+    if(isinstance(cday, list)):
+        idx = ecobeeData["Datetime"].transform(lambda x: x >= cday[0] and x <= cday[1])
+    else:
+        idx = ecobeeData["Datetime"].transform(lambda x: x.month==cday.month and x.day == cday.day)
+    return ecobeeData.loc[idx]
+
+def plot_ecobee_data(ecobeeData):
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(x=ecobeeData["Datetime"], y=ecobeeData["Current Temp (F)"], name="Main", mode="markers+lines"))
+    fig.add_trace(go.Scatter(x=ecobeeData["Datetime"], y=ecobeeData["Outdoor Temp (F)"], name="Outdoor", mode="markers+lines"))
+    fig.add_trace(go.Scatter(x=ecobeeData["Datetime"], y=ecobeeData["Bedroom (F)"], name="Bedroom", mode="markers+lines"))
+    fig.add_trace(go.Scatter(x=ecobeeData["Datetime"], y=ecobeeData["Sunroom (F)"], name="Sunroom", mode="markers+lines"))
+    return fig
